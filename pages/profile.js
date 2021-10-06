629b484f7fc04eb9b1a61bda27eb4979eb784196
Walker <kimaniwalker@gmail.com>
2021-10-06 16:50:38 -0500

    Modified profile page

--- pages/profile.js
+++ pages/profile.js
@@ -10,6 +10,7 @@ import Twofactorauthsetup from '../components/twofactorauthsetup'
 const Profile = () => {
   const user = useUser({ redirectTo: '/login' })
   const [customer, setcustomer] = React.useState([])
+  const [status, setstatus] = React.useState('')
 
   const getcustomerinfo = async () => {
     let body = {
@@ -25,6 +26,7 @@ const Profile = () => {
       if (res.status === 200) {
         const session = await res.json()
         setcustomer(session)
+        setstatus(session.customer.subscriptions.data[0].status)
 
 
       } else {
@@ -48,9 +50,10 @@ const Profile = () => {
   return (
     <Layout>
 
-      {user && (
+      {user &&  (
         <>
           <h1 className="py-4">My Profile</h1>
+          
           <Profilecard title={'Manage Subscription'} body={'Cancel your subscription anytime. Use this portal to view and manage your subscription information.'} image={<svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-credit-card-2-back" viewBox="0 0 16 16">
             <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
             <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
@@ -61,13 +64,15 @@ const Profile = () => {
             <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415z" />
           </svg>} stripe={<Twofactorauthsetup user={user} />} />
 
-          {customer.customer.subscriptions.data[0].status === 'active' ? <Profilecard title={'Add New Child'} body={'Add and modify your children\'s demographic information here. Don\'t forget to update it every 3-4 months !'} image={<svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+         
+          {status === 'active' ? <Profilecard title={'Add New Child'} body={'Add and modify your children\'s demographic information here. Don\'t forget to update it every 3-4 months !'} image={<svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
             <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
             <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
           </svg>} stripe={<Stripebilling user={user} />} /> : null}
+          
 
 
-          <p>Your session:</p>
+          
           <pre>{JSON.stringify(user, null, 2)}</pre>
         </>
       )}
