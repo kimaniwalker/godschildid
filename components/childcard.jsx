8614a12a6d1148078b25c8831395214cebd90e64
Walker <kimaniwalker@gmail.com>
2021-10-06 22:07:09 -0500

    Added card component

--- components/childcard.jsx
+++ components/childcard.jsx
@@ -5,19 +5,65 @@ export default function Childcard(props) {
 
     return (
         <>
-            <div class="card" style={{ width: '18rem' }}>
-                <img src={props.image} class="card-img-top" alt="profile pic" />
+            <div class="card w-50">
+
                 <div class="card-body">
                     <div className="row">
-
-
-                        <div className="card-title">
-                            {props.first_name} {props.last_name}
+                        <div className="col-5">
+                            <img src={props.image} class="card-img" alt="profile pic" />
                         </div>
+                        <div className="col-7">
+                            <div className="card-title">
+                                <h4>{props.first_name} {props.last_name}</h4>
+                                <p>Age: {props.age}</p>
+                                <p>Gender: {props.gender}</p>
+                                <p>D.O.B: {props.dob}</p>
+
+
+                            </div>
+                        </div>
+
+
+
 
 
                     </div>
 
+                    <div className="row pt-2">
+                        <div className="col-6">
+                            <p>Height: {props.height}</p>
+                            <p>Weight: {props.weight}</p>
+
+
+
+                        </div>
+                        <div className="col-6">
+                            <p>Hair Color: {props.hair}</p>
+                            <p>Eye Color: {props.eyes}</p>
+
+
+
+                        </div>
+                    </div>
+                    <div className="row">
+                        <p>Medical Conditions: {props.medical_conditions}</p>
+                        <p>Identifying Marks: {props.marks}</p>
+                    </div>
+
+                </div>
+                <div className="card-footer bg-primary">
+                    <div className="row text-center text-secondary">
+                      <div className="col-4">
+                    Browse
+                </div>
+                <div className="col-4">
+                    Flier
+                </div>
+                <div className="col-4">
+                    Edit
+                </div>  
+                    </div>
+                
                 </div>
             </div>
         </>
---
+++ pages/mypage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Childcard from '../components/childcard'
+import Layout from '../components/layout'
+
+export default function Mypage(props) {
+    
+
+    return (
+        <>
+        <Layout>
+            
+            <Childcard first_name="Kimani" last_name="Walker" height="5\'2" weight="135" gender="Male" dob="10-15-1994" age="27" hair="black" eyes="brown" medical_conditions="No existing medical conditions" marks="Large tatoo on right arm and chest" image="https://daisybeattyphotography.com/wp-content/uploads/2015/09/15-1790-post/head-shots-for-kids-new-york-daisy-beatty(pp_w768_h915).jpg"/>
+          
+            
+           
+        </Layout>
+            
+        </>
+    )
+}
