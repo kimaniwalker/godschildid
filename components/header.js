c618bf713c3f9c9a8e687e10b0779d9be0444f7c
esmith-33 <67183435+esmith-33@users.noreply.github.com>
2021-10-28 21:52:26 -0500

    Update header.js
    Made the navbar responsive

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 import Topheader from './topheader'
 import styles from '../styles/css/header.module.css'
 import { useAppContext } from '../lib/context/userstate'
-
+import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
 
 const Header = () => {
   const user = useAppContext()
@@ -13,34 +13,23 @@ const Header = () => {
 
         ?
 
-        <nav id={styles.header} class="navbar navbar-expand-lg sticky-top navbar-light bg-primary2 py-4">
+<Navbar bg="primary2" variant="dark" expand="lg" sticky="top">
+  <Container>
+    {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="container-fluid d-flex justify-content-center">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/faq">Faq</Nav.Link>
+        <Nav.Link href="/links">Important Links</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav.Link href="/login">Login</Nav.Link>
 
-          <div class="container-fluid d-flex justify-content-center">
-            <ul class="navbar-nav ">
-              <li class="nav-item">
-                <a class={styles.navLink} aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class={styles.navLink} href="/faq">Faq</a>
-              </li>
-              <li class="nav-item">
-                <a class={styles.navLink} href="/links">Important Links</a>
-              </li>
-              <li class="nav-item">
-                <a class={styles.navLink} href="/about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class={styles.navLink} href="/contact">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a class={styles.navLink} href="/login">Login</a>
-              </li>
-            </ul>
-
-          </div>
-
-
-        </nav>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
         :
 
