ae19e6e49c1f86adb36644e25f6586f53601c49b
esmith-33 <67183435+esmith-33@users.noreply.github.com>
2021-11-08 19:21:35 -0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -39,11 +39,11 @@ const Header = () => {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="container-fluid d-flex justify-content-center">
         <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/mypage">Six Up</Nav.Link>
-        <Nav.Link href="/faq">Faq</Nav.Link>
+        <Nav.Link href="/mypage">My Children</Nav.Link>
+        <Nav.Link href="/faq">FAQ</Nav.Link>
         <Nav.Link href="/links">Important Links</Nav.Link>
         <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/profile">Profile</Nav.Link>
+        <Nav.Link href="/profile">Settings</Nav.Link>
         <Nav.Link href="/contact">Contact</Nav.Link>
         <Nav.Link href="/api/auth/logout">Logout</Nav.Link>
 
