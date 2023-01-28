d8c52d461d5a3a2d26d2e1539d23c156f2465607
kimaniwalker <kimaniwalker@gmail.com>
2023-01-27 20:37:09 -0600

    [feat] - update codes

--- data/codes.jsx
+++ data/codes.jsx
@@ -4,7 +4,19 @@ export const Codes = [
     'HELENA22',
     'GODSCHILDAL22',
     'godschildal22',
-    'Godschildal22'
+    'Godschildal22',
+    'MPL14',
+    'mpl14',
+    'ISB14',
+    'isb14',
+    'CLN14',
+    'cln14',
+    'VRB14',
+    'vrb14',
+    'TRB14',
+    'trb14',
+    'JMN14',
+    'jmn14'
 ]
 
 
